9640c8971c8fca51aab40b4d8b778c353c907154
Timur Batyrshin <erthad@gmail.com>
2021-12-24 17:30:38 +0300

    refactor youtube-dl-worker

---
+++ lib/youtube_dl/helpers/local_video.rb
@@ -0,0 +1,19 @@
+module LocalVideo
+  # Safely run command with error checking and logging
+  def run_command(command, description)
+    Hanami.logger.info description
+
+    stdin, stdout, stderr, wait_thr = Open3.popen3(command)
+    exit_status = wait_thr.value
+    lines_stdout = stdout.read
+    lines_stderr = stderr.read
+
+    if exit_status != 0
+      Hanami.logger.error "Exit status: #{exit_status}"
+      Hanami.logger.debug "STDOUT: #{lines_stdout}"
+      Hanami.logger.debug "STDERR: #{lines_stderr}"
+    end
+
+    [lines_stdout.chomp, lines_stderr, exit_status]
+  end
+end
--- lib/youtube_dl/workers/youtube_dl_worker.rb
+++ lib/youtube_dl/workers/youtube_dl_worker.rb
@@ -2,6 +2,7 @@ require "open3"
 
 class YoutubeDlWorker
   include Sidekiq::Worker
+  include LocalVideo
 
   def perform(arg)
     repo = VideoRepository.new
@@ -30,33 +31,12 @@ class YoutubeDlWorker
     Hanami.logger.info "Finished downloading video #{video.url} to #{video.location.path}"
   end
 
-  def download_video(video, location)
-    _, _, exit_status = run_command("youtube-dl #{video}", "Downloading video #{video} to #{location}")
-    exit_status
-  end
-
   def update_info(video)
-    id, _, exit_status = run_command("youtube-dl --get-id #{video.url}", "Getting id for  #{video.url}")
-    return if exit_status !=0
-
-    _, _, exit_status = run_command(
-      "youtube-dl --write-info-json --skip-download --id #{video.url}",
-      "Getting info for  #{video.url}"
-     )
-    return if exit_status !=0
-
-    json = JSON.parse(File.read("#{id}.info.json"))
-    File.delete("#{id}.info.json")
-
-    if !json
-      raise "Error processing JSON for #{video}"
-    end
-
     video_repo = VideoRepository.new
-    info_repo = VideoInfoRepository.new
 
-    video_info = info_repo.find(youtube_id: json['id'])
+    json = get_info(video.url)
     new_info = VideoInfo.create_from_youtube(json)
+
     if video.video_info
       video_repo.update_video_info(video, new_info)
     else
@@ -64,17 +44,21 @@ class YoutubeDlWorker
     end
   end
 
-  def run_command(command, description)
-    Hanami.logger.info description
-    stdin, stdout, stderr, wait_thr = Open3.popen3(command)
-    exit_status = wait_thr.value
-    lines_stdout = stdout.read
-    lines_stderr = stderr.read
-    if exit_status != 0
-      Hanami.logger.error "Exit status: #{exit_status}"
-      Hanami.logger.debug "STDOUT: #{lines_stdout}"
-      Hanami.logger.debug "STDERR: #{lines_stderr}"
-    end
-    [lines_stdout.chomp, lines_stderr, exit_status]
+  def get_info(video)
+    id, _, exit_status = run_command("youtube-dl --get-id #{video}", "Getting id for  #{video}")
+    return nil if exit_status !=0
+
+    _, _, exit_status = run_command("youtube-dl --write-info-json --skip-download --id #{video}", "Getting info for  #{video}")
+    return nil if exit_status !=0
+
+    json = JSON.parse(File.read("#{id}.info.json"))
+    File.delete("#{id}.info.json")
+
+    return json
+  end
+
+  def download_video(video, location)
+    _, _, exit_status = run_command("youtube-dl #{video}", "Downloading video #{video} to #{location}")
+    exit_status
   end
 end
